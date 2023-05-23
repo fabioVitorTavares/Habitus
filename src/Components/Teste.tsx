57bf20a9a3d818f78b1c317d3b8296231ca24a81
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-05-22 23:24:24 -0300

    commit

---
+++ src/Components/Teste.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Teste() {
+
+
+
+  return (
+    <View>
+      <Text>
+        Teste Habitus
+      </Text>
+    </View>
+  )
+}
