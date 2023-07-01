fe6b92de0249b7f3f6039b2b87ca1c61d14bded8
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-07-01 20:33:19 -0300

    commit

--- src/Screens/Authenticate/Authenticate.tsx
+++ src/Screens/Authenticate/Authenticate.tsx
@@ -2,17 +2,55 @@ import { Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { NavigationProp } from "@react-navigation/native";
 import ScreenContainer from "../../Components/ScreenContainer/ScreenContainer";
+import { useState } from "react";
 
 type NavigationType = {
   navigation: NavigationProp<ReactNavigation.RootParamList>;
 };
 
 export default function Authenticate({ navigation }: NavigationType) {
+  const [pin, setPin] = useState("");
+
+  const Visor = () => {
+    return (
+      <View>
+        <Text>{pin}</Text>
+      </View>
+    );
+  };
+
+  const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, "C", 0, "<"];
+
+  const Touchs = () => {
+    return (
+      <View>
+        {numbers.map((n) => {
+          return (
+            <TouchableOpacity
+              key={`${Math.random()}${n}`}
+              onPress={() =>
+                n === "<"
+                  ? setPin((prev) => prev.slice(0, prev.length - 1))
+                  : n == "C"
+                  ? setPin("")
+                  : setPin((prev) => `${prev}${n}`)
+              }
+            >
+              <Text>{n}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
     <ScreenContainer>
       <View>
         <TouchableOpacity onPress={() => navigation.navigate("App")}>
           <Text>Authenticate</Text>
+          <Visor />
+          <Touchs />
         </TouchableOpacity>
       </View>
     </ScreenContainer>
