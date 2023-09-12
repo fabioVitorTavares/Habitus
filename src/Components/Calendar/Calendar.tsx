88eac17ecc9983be6b89015dfba8c556e55fe896
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-09-11 22:40:52 -0300

    commit

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -1,46 +1,63 @@
 import { Pressable, Text, View } from "react-native";
 import { CalendarProps } from "../../Types/Types";
 import { styles } from "./CalendarStyles";
-import {
-  GestureEvent,
-  PanGestureHandler,
-  PanGestureHandlerEventPayload,
-  TouchableOpacity,
-} from "react-native-gesture-handler";
-import { useAnimatedGestureHandler } from "react-native-reanimated";
+import Carousel from "react-native-reanimated-carousel";
+import { useEffect, useState } from "react";
+
 const ONE_DAY_IN_MS = 86400000;
 export default function Calendar({
   currentDate,
   setCurrentDate,
 }: CalendarProps) {
+  const [prevCurrentNextDay, setPrevCurrentNextDay] = useState<Date[]>([]);
+
+  useEffect(() => {
+    setPrevCurrentNextDay([previousDate(), currentDate, nextDate()]);
+  }, [currentDate]);
+
   function nextDay() {
     setCurrentDate((p) => new Date(p.getTime() + ONE_DAY_IN_MS));
   }
+
   function previousDay() {
     setCurrentDate((p) => new Date(p.getTime() - ONE_DAY_IN_MS));
   }
 
-  const onDrag = useAnimatedGestureHandler<
-    GestureEvent<PanGestureHandlerEventPayload>,
-    Record<string, number>
-  >({
-    onStart: (event, context) => {
-      console.log("Log line 28: ", event);
-    },
-    onActive: (event, context) => {},
-  });
+  function nextDate() {
+    return new Date(currentDate.getTime() + ONE_DAY_IN_MS);
+  }
+
+  function previousDate() {
+    return new Date(currentDate.getTime() - ONE_DAY_IN_MS);
+  }
+
+  function changeIndexCarousel(index: number) {
+    console.log("Log line 37: ", prevCurrentNextDay);
+  }
 
   return (
     <View style={styles.caledarContainer}>
-      <PanGestureHandler onGestureEvent={onDrag} activeOffsetX={-50}>
-        <Text>{currentDate.toLocaleDateString()}</Text>
-        {/* <TouchableOpacity onPress={nextDay}>
-          <Text>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={previousDay}>
-          <Text>-</Text>
-        </TouchableOpacity> */}
-      </PanGestureHandler>
+      <Carousel
+        width={200}
+        height={100}
+        loop
+        data={prevCurrentNextDay}
+        scrollAnimationDuration={500}
+        onSnapToItem={(index) => changeIndexCarousel(index)}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 30 }}>
+              {item.toLocaleDateString()}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
--- src/Components/Calendar/CalendarStyles.ts
+++ src/Components/Calendar/CalendarStyles.ts
@@ -1,9 +1,19 @@
 import { StyleSheet } from "react-native";
+import { fullSize, centerItens } from "../../Styles/DefaultsStyles";
 
 export const styles = StyleSheet.create({
   caledarContainer: {
     width: '90%',
     height: 300,
     borderWidth: 1
+  },
+  animatedTouchable: {
+    ...fullSize,
+  },
+  carouselDay: {
+    backgroundColor: '#900',
+    width: 500,
+    height: 100,
+    
   }
 });
